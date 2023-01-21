9a99323724042135ef558898862c51a251271845
StrategySam <thestrategysam@gmail.com>
2023-01-20 18:41:52 -0600

    COLOR TIME :))

---
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -0,0 +1,24 @@
+
+package frc.robot.subsystems;
+import com.revrobotics.ColorSensorV3;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ColorSensor extends SubsystemBase {
+
+
+    private final static I2C.Port i2cPort = I2C.Port.kOnboard;
+    static ColorSensorV3 sensor = new ColorSensorV3(i2cPort);
+    private static int[] color = new int[3];
+    public static int[] getColor() {
+        color[0] = sensor.getRed();
+        color[1] = sensor.getGreen();
+        color[2] = sensor.getBlue();
+        return color;
+    }
+
+}
+
